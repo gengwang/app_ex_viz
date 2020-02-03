30e37bd7c4e6d444c24fda247f25ac650cd6851d
Geng Wang <gwangdesign@yahoo.com>
2020-02-02 18:50:33 -0800

    Add SingleAppSingleSite.R

---
+++ single_app_singe_site_sim.R
@@ -0,0 +1,63 @@
+library(statsr)
+library(dplyr)
+library(ggplot2)
+library(purrr)
+# library(scales)
+# library(xts)
+
+# Sample size we need for our viz: sample very 5 min in two days
+score_range <- factor(c(1:10), ordered = TRUE)
+t <- seq(ISOdatetime(2020,1,30,0,0,0), ISOdatetime(2020,2,1,0,0,0), by=(60*5))
+sample.size_total <- length(t)
+score <-  sample(1:10, sample.size_total, replace = T)
+health_o_t <- data.frame(score = score, time = t)
+health_o_t <- health_o_t %>% mutate(class = score %>% bin_10_to_3())
+health_o_t <- health_o_t[c('time', 'score', 'class')]
+
+# shatter an interger into several integers so that the sum of which equals the original number
+# for example, shatter(577) may return c(190, 204, 183) 
+shatter <- function(x, size=3) {
+  shatter <- sample(1:round(size), round(x), replace = T) %>% factor() %>%  summary()
+}
+# generate a number of size of random integers, starting with start. This is a "smoother" alternative of sample function
+ran_smoother <- function(start=5, size=10, range=2) {
+  # 1:3 %>% reduce(pile) will produce c(1, 3, 6)
+  pile <- function(acc, d) {
+    last <- acc %>% last() + d
+    acc <- acc %>% append(last)
+  }
+  s <- sample((-1*range):range, size, replace=T)
+  ran_smoother <- s %>% reduce(pile) + start
+}
+
+bin_10_to_3 <- function(x) {
+  case_when(
+    x <= 4 ~ "Poor",
+    x <= 7 & x >4 ~ "Fair",
+    x > 7 ~ "Good"
+  )
+}
+
+# manually generate some health scores for 2 days in an interval of 5 mins
+n <- 24
+h <- ran_smoother(size=n) %>% sapply(min, max(score_range)) %>% sapply(max, min(score_range))
+s <- shatter(sample.size_total, n)
+h_s <- data.frame(health = h, size=s)
+make_fregments <- function(x, out) {
+  out <- rep(x[1], x[2])
+}
+
+sample.health_scores <- apply(h_s, 1, make_fregments) %>% unlist()
+sample.health_scores <- data.frame( time = t, score = sample.health_scores)
+sample.health_scores <- sample.health_scores %>% mutate(cls = bin_10_to_3(score))
+write.csv(sample.health_scores, './assets/001.csv', append = T)
+write.csv(sample.health_scores, './assets/health_scores.csv')
+
+# # # # # # # # # # # # # # # # # # # # # # # 
+# visualization
+sample2.health_scores <- read.csv('./assets/health_scores_1.csv', stringsAsFactors = F)
+sample2.health_scores <- sample2.health_scores %>% mutate(time = time %>% as.POSIXct())
+p_score <- ggplot(data=sample2.health_scores, aes(x=time, y=score))+
+  geom_line()
+ggplot(data=sample2.health_scores, aes(x=time, y=cls))+
+  geom_point()
